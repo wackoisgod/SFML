1d2634917d33a38dda69423b92f9efacf872a09a
Jasson McMorris <jasson@siegegames.com>
2016-01-17 18:08:14 -0800

    Limit joysticks to 4 since this can cause crashes on some systems

--- include/SFML/Window/Joystick.hpp
+++ include/SFML/Window/Joystick.hpp
@@ -48,7 +48,7 @@ public:
     ////////////////////////////////////////////////////////////
     enum
     {
-        Count       = 8,  ///< Maximum number of supported joysticks
+        Count       = 4,  ///< Maximum number of supported joysticks
         ButtonCount = 32, ///< Maximum number of supported buttons
         AxisCount   = 8   ///< Maximum number of supported axes
     };
